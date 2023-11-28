535f4eceb21dfb7746235821370ea0c1d18abce8
Connor Lindsey <cblindsey3@gmail.com>
2023-11-28 11:16:08 -0800

    rename ts-config and eslint config to @knocklabs/*

--- packages/react-core/.eslintrc.js
+++ packages/react-core/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
+  extends: ["@knocklabs/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
