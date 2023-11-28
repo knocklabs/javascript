69eee30f5d143c7f6bfbba2d30de495853e7b589
Connor Lindsey <cblindsey3@gmail.com>
2023-11-28 10:11:32 -0800

    build esm and cjs correctly

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -7,15 +7,21 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-
-  const CJS = env.NX_TASK_TARGET_TARGET?.toLocaleLowerCase()?.match("cjs");
-  // const ESM = !CJS;
+  const CJS = env.BUILD_TARGET?.toLocaleLowerCase()?.match("cjs");
   let formats: LibraryFormats[] = ["es"];
   if (CJS) formats = ["cjs"];
 
   return {
-    plugins: [dts(), react(), noBundlePlugin({ root: "./src" })],
+    plugins: [
+      dts({
+        outDir: "dist/types",
+      }),
+      react(),
+      noBundlePlugin({ root: "./src" }),
+    ],
     build: {
+      outDir: CJS ? "dist/cjs" : "dist/esm",
+      sourcemap: true,
       lib: {
         entry: resolve(__dirname, "src/index.ts"),
         name: "react",
@@ -28,20 +34,14 @@ export default defineConfig(({ mode }) => {
           format: formats[0],
           assetFileNames: (assetInfo) => {
             // Rename styles to index.css
-            if (assetInfo.name === "style.css") return "index.css";
+            if (assetInfo.name === "style.css") {
+              return "index.css";
+            }
             return assetInfo.name;
           },
         },
         // External packages that should not be bundled
-        external: [
-          "react",
-          "react-dom",
-          "react/jsx-runtime",
-          "@knocklabs/client",
-          "date-fns",
-          "react",
-          "zustand",
-        ],
+        external: ["react", "react-dom"],
       },
     },
   };
