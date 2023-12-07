94b7fdff1db691a8ed36b385aafe5ec576b6f72a
Connor Lindsey <cblindsey3@gmail.com>
2023-12-07 10:44:18 -0800

    fix: remove extra css imports from bundled components

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import noBundlePlugin from "vite-plugin-no-bundle";
 import dts from "vite-plugin-dts";
-import del from "rollup-plugin-delete";
 import execute from "rollup-plugin-execute";
 
 // https://vitejs.dev/config/
@@ -43,12 +42,28 @@ export default defineConfig(({ mode }) => {
         // External packages that should not be bundled
         external: ["react", "react-dom"],
         plugins: [
+          execute([
+            // Move index.css to root of dist
+            `mv dist/esm/index.css dist/index.css`,
+            // Delete extra .css.js files
+            `rm dist/**/*.css.*`,
+          ]),
           // Remove css imports
-          // TODO
-          // Delete extra .css.js files
-          del({ targets: "dist/**/*.css.*" }),
-          // Move index.css to root of dist
-          execute(["mv dist/esm/index.css dist/index.css"]),
+          {
+            name: "remove-css-imports",
+            generateBundle(_options, bundle) {
+              for (const fileName in bundle) {
+                const file = bundle[fileName];
+
+                if (file.type === "chunk") {
+                  // Replace .css imports and requires
+                  const pattern =
+                    /(import\s+['"].+\.css(\.mjs)?['"];?)|(require\(['"].+\.css(\.js)?['"]\);?)/;
+                  file.code = file.code.replace(pattern, "");
+                }
+              }
+            },
+          },
         ],
       },
     },
