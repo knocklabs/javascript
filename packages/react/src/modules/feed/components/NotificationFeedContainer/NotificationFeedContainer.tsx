d80d7127aa5ea27afbd5d4b0ced06b955ec4af81
Connor Lindsey <cblindsey3@gmail.com>
2024-01-05 12:34:01 -0800

    fix naming in component file

--- packages/react/src/modules/feed/components/NotificationFeedContainer/NotificationFeedContainer.tsx
+++ packages/react/src/modules/feed/components/NotificationFeedContainer/NotificationFeedContainer.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 import "./styles.css";
 
-export const KnockFeedContainer: React.FC<PropsWithChildren> = ({
+export const NotificationFeedContainer: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   return <div className="rnf-feed-provider">{children}</div>;
