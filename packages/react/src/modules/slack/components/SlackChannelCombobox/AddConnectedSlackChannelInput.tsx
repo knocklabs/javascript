46f36335529cddf6b7dd7b583791b1ae04e41612
Meryl Dakin <meryl.k.dakin@gmail.com>
2024-02-15 17:32:34 -0500

    feat(kno-5241): add channel id text box for adding connected channels (#62)

---
+++ packages/react/src/modules/slack/components/SlackChannelCombobox/AddConnectedSlackChannelInput.tsx
@@ -0,0 +1,72 @@
+import { SlackChannelConnection } from "@knocklabs/client";
+import { useState } from "react";
+
+import { SlackIcon } from "../SlackIcon";
+
+import ConnectionErrorInfoBoxes from "./ConnectionErrorInfoBoxes";
+import "./styles.css";
+
+const AddConnectedSlackChannelInput = ({
+  isErrorState,
+  connectedChannels = [],
+  updateConnectedChannels,
+  connectedChannelsError,
+  connectedChannelsUpdating,
+}: {
+  isErrorState: boolean;
+  connectedChannels: SlackChannelConnection[];
+  updateConnectedChannels: (channels: SlackChannelConnection[]) => void;
+  connectedChannelsError: string | null;
+  connectedChannelsUpdating: boolean;
+}) => {
+  const [value, setValue] = useState<string | null>(null);
+  const [localError, setLocalError] = useState<string | null>(null);
+
+  const submitChannel = () => {
+    if (!value) {
+      return;
+    }
+
+    if (localError) {
+      setLocalError(null);
+    }
+
+    const alreadyConnected = connectedChannels.find(
+      (channel) => channel.channel_id === value,
+    );
+
+    if (alreadyConnected) {
+      setValue("");
+      return setLocalError(`"${value}" already connected.`);
+    }
+
+    const channelsToSendToKnock = [...connectedChannels, { channel_id: value }];
+    updateConnectedChannels(channelsToSendToKnock);
+    setValue("");
+  };
+
+  return (
+    <div className="rnf-connect-channel-input-container">
+      <input
+        className={`rnf-input ${((isErrorState || !!localError) && !value) && "rnf-input-error"}`}
+        tabIndex={-1}
+        id="slack-channel-search"
+        type="text"
+        placeholder={localError || connectedChannelsError || "Slack channel ID"}
+        onChange={(e) => setValue(e.target.value)}
+        value={value || ""}
+      />
+      <button className="rnf-button" onClick={submitChannel}>
+        {connectedChannelsUpdating ? (
+          <div className="rnf-spinner"></div>
+        ) : (
+          <SlackIcon height="16px" width="16px" />
+        )}
+        Connect channel
+      </button>
+      <ConnectionErrorInfoBoxes />
+    </div>
+  );
+};
+
+export default AddConnectedSlackChannelInput;
