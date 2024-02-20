e9665e0af1ff58344f69524a0dec9bdfb4d8ac64
Meryl Dakin <meryl.k.dakin@gmail.com>
2024-02-20 15:57:39 -0500

    feat(kno-5254): hide inaccessible channels

--- packages/react/src/modules/slack/components/SlackChannelCombobox/icons/CheckmarkIcon.tsx
+++ packages/react/src/modules/slack/components/SlackChannelCombobox/icons/CheckmarkIcon.tsx
@@ -1,10 +1,19 @@
-const CheckmarkIcon = ({ isConnected }: { isConnected: boolean }) => (
+const CheckmarkIcon = ({
+  isConnected,
+  size = "1rem",
+  ...props
+}: {
+  isConnected: boolean;
+  size?: string;
+}) => (
   <svg
-    width="14"
-    height="15"
+    height={size}
+    width={size}
+    role="img"
     viewBox="0 0 14 15"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       d="M11.3751 3.9996L5.25006 10.9996L2.62506 8.3746"
