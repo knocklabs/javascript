ab12999af28f9477c81ef6568e441f99b4b7a1d7
Meryl Dakin <meryl.k.dakin@gmail.com>
2024-02-28 14:48:37 -0500

    update

--- examples/slack-kit-example/app/components/slack-channel-wrapper.tsx
+++ examples/slack-kit-example/app/components/slack-channel-wrapper.tsx
@@ -6,33 +6,33 @@ import { useState } from "react";
 import ToggleSwitch from "./toggle-switch";
 
 export default function SlackChannelWrapper({
-	collection,
-	objectId,
-	className,
+  collection,
+  objectId,
+  className,
 }: {
-	collection: string;
-	objectId: string;
-	className: string;
+  collection: string;
+  objectId: string;
+  className: string;
 }) {
-	const [showConnectedChannels, setShowConnectedChannels] = useState(false);
+  const [showConnectedChannels, setShowConnectedChannels] = useState(false);
 
-	const slackChannelsRecipientObject = {
-		objectId: objectId,
-		collection: collection,
-	};
-	return (
-		<div className={className}>
+  const slackChannelsRecipientObject = {
+    objectId: objectId,
+    collection: collection,
+  };
+  return (
+    <div className={className}>
       <div className="mb-3">
-			<ToggleSwitch
-				label="Show connected channels"
-				isToggled={showConnectedChannels}
-				setIsToggled={setShowConnectedChannels}
-			/>
+      <ToggleSwitch
+        label="Show connected channels"
+        isToggled={showConnectedChannels}
+        setIsToggled={setShowConnectedChannels}
+      />
       </div>
-			<SlackChannelCombobox
-				slackChannelsRecipientObject={slackChannelsRecipientObject}
-				showConnectedChannelTags={showConnectedChannels}
-			/>
-		</div>
-	);
+      <SlackChannelCombobox
+        slackChannelsRecipientObject={slackChannelsRecipientObject}
+        showConnectedChannelTags={showConnectedChannels}
+      />
+    </div>
+  );
 }
