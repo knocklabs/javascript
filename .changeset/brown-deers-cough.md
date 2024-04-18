b8b3fc96ce8d1a7e17890e9c824120c955dec637
Chris Bell <cjbell1988@gmail.com>
2024-04-18 10:03:48 -0400

    fix: don't use propswithchildren w/o props (#122)

---
+++ .changeset/brown-deers-cough.md
@@ -0,0 +1,6 @@
+---
+"@knocklabs/react-native": patch
+"@knocklabs/react": patch
+---
+
+fix: don't use propswithchildren w/o props
--- packages/react-native/src/modules/feed/components/NotificationFeedContainer/NotificationFeedContainer.tsx
+++ packages/react-native/src/modules/feed/components/NotificationFeedContainer/NotificationFeedContainer.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import { FunctionComponent, ReactNode } from "react";
 import { View } from "react-native";
 
-export const NotificationFeedContainer: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => {
-  return <View>{children}</View>;
-};
+export const NotificationFeedContainer: FunctionComponent<{
+  children?: ReactNode | undefined;
+}> = ({ children }) => <View>{children}</View>;
