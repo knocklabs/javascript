28ac6cd19805ecc0527dfce42ce342a2281be12c
Matthew Mikolay <mmikolay@knock.app>
2024-12-12 16:31:17 -0500

    Add openPopupWindow helper

--- packages/react/src/modules/ms-teams/components/MSTeamsAuthButton/MSTeamsAuthButton.tsx
+++ packages/react/src/modules/ms-teams/components/MSTeamsAuthButton/MSTeamsAuthButton.tsx
@@ -1,6 +1,7 @@
 import { useMSTeamsAuth } from "@knocklabs/react-core";
 import { FunctionComponent } from "react";
 
+import { openPopupWindow } from "../../../core/utils";
 import { SlackIcon } from "../../../slack/components/SlackIcon";
 
 export interface MSTeamsAuthButtonProps {
@@ -8,30 +9,6 @@ export interface MSTeamsAuthButtonProps {
   redirectUrl?: string;
   onAuthenticationComplete?: (authenticationResp: string) => void;
 }
-
-// TODO Consolidate
-const openMSTeamsOauthPopup = (url: string) => {
-  const width = 600;
-  const height = 800;
-  // Calculate the position to center the window
-  const screenLeft = window.screenLeft ?? window.screenX;
-  const screenTop = window.screenTop ?? window.screenY;
-
-  const innerWidth =
-    window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
-  const innerHeight =
-    window.innerHeight ??
-    document.documentElement.clientHeight ??
-    screen.height;
-
-  const left = innerWidth / 2 - width / 2 + screenLeft;
-  const top = innerHeight / 2 - height / 2 + screenTop;
-
-  // Window features
-  const features = `width=${width},height=${height},top=${top},left=${left}`;
-
-  window.open(url, "MS Teams OAuth", features);
-};
 
 export const MSTeamsAuthButton: FunctionComponent<MSTeamsAuthButtonProps> = ({
   msTeamsBotId,
@@ -42,7 +19,7 @@ export const MSTeamsAuthButton: FunctionComponent<MSTeamsAuthButtonProps> = ({
 
   return (
     <button
-      onClick={() => openMSTeamsOauthPopup(buildMSTeamsAuthUrl())}
+      onClick={() => openPopupWindow(buildMSTeamsAuthUrl())}
       className="rsk-connect__button rsk-connect__button--disconnected"
     >
       <SlackIcon height="16px" width="16px" />
