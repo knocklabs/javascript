df64bbab1f3ff71aab2c9c9d9b159e759f1e70b3
Matthew Mikolay <mmikolay@knock.app>
2024-12-12 14:42:43 -0500

    Render KnockProvider in example app

--- examples/ms-teams-connect-example/pages/index.tsx
+++ examples/ms-teams-connect-example/pages/index.tsx
@@ -1,3 +1,31 @@
+import { KnockProvider } from "@knocklabs/react";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const user = {
+    id: "123",
+    email: "",
+    name: "",
+    phone_number: "",
+    avatar: "",
+    updated_at: "",
+    created_at: "",
+  };
+
+  return (
+    <KnockProvider
+      apiKey={process.env.NEXT_PUBLIC_KNOCK_CLIENT_ID!}
+      userId={user.id}
+      host={process.env.NEXT_PUBLIC_KNOCK_API_URL}
+    >
+      <div
+        style={{
+          marginBottom: "20px",
+          fontFamily: "monospace",
+          fontSize: "40px",
+        }}
+      >
+        MS Teams connector examples
+      </div>
+    </KnockProvider>
+  );
 }
