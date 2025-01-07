44b2e6fe492e5558e9dd23dd3c346a42e0a17202
Matthew Mikolay <mmikolay@knock.app>
2025-01-07 16:56:29 -0500

    Add ms-teams-connect-example

---
+++ examples/ms-teams-connect-example/pages/_app.tsx
@@ -0,0 +1,26 @@
+import "@knocklabs/react/dist/index.css";
+import { NextSeo } from "next-seo";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NextSeo
+        title="MS Teams connector components example | Powered by Knock"
+        description="MS Teams connector components, powered by Knock"
+        openGraph={{
+          title: "MS Teams connector components example | Powered by Knock",
+          description: "MS Teams connector components, powered by Knock",
+        }}
+        twitter={{
+          handle: "@knocklabs",
+          site: "@knocklabs",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
