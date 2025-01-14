3a2e7e948a32721b40f424ca9f57fd5866fac9d7
Matthew Mikolay <mmikolay@knock.app>
2025-01-14 13:35:46 -0500

    Update types

--- packages/client/src/clients/ms-teams/interfaces.ts
+++ packages/client/src/clients/ms-teams/interfaces.ts
@@ -10,6 +10,16 @@ export type GetMsTeamsTeamsInput = {
   };
 };
 
+export type GetMsTeamsChannelsInput = {
+  tenantId: string;
+  knockChannelId: string;
+  teamId: string;
+  queryOptions?: {
+    $filter?: string;
+    $select?: string;
+  };
+};
+
 export type GetMsTeamsTeamsResponse = {
   ms_teams_teams: MsTeamsTeam[];
   next_link: string | null;
