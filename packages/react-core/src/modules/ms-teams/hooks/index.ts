546473c0afcea7a1821f3dd9345c6a72d99cfd43
Matthew Mikolay <mmikolay@knock.app>
2025-01-16 12:33:37 -0500

    Update exports

--- packages/react-core/src/modules/ms-teams/hooks/index.ts
+++ packages/react-core/src/modules/ms-teams/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useMsTeamsConnectionStatus } from "./useMsTeamsConnectionStatus";
 export { default as useMsTeamsAuth } from "./useMsTeamsAuth";
 export { default as useMsTeamsTeams } from "./useMsTeamsTeams";
+export { default as useMsTeamsChannels } from "./useMsTeamsChannels";
---
+++ packages/react-core/src/modules/ms-teams/hooks/useMsTeamsChannels.ts
@@ -0,0 +1,7 @@
+import { useKnockClient } from "../../core";
+
+function useMsTeamsChannels() {
+  const knock = useKnockClient();
+}
+
+export default useMsTeamsChannels;
