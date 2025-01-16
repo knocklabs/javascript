8f869d99d6a34aaf690bf08d37c102cfc661a745
Matthew Mikolay <mmikolay@knock.app>
2025-01-16 13:12:28 -0500

    Implement useMsTeamsChannels

--- packages/react-core/src/modules/ms-teams/hooks/useMsTeamsChannels.ts
+++ packages/react-core/src/modules/ms-teams/hooks/useMsTeamsChannels.ts
@@ -1,7 +1,44 @@
-import { useKnockClient } from "../../core";
+import { GetMsTeamsChannelsResponse, MsTeamsChannel } from "@knocklabs/client";
+import useSWR from "swr";
 
-function useMsTeamsChannels() {
+import { useKnockClient } from "../../core";
+import { useKnockMsTeamsClient } from "../context";
+
+const QUERY_KEY = "MS_TEAMS_CHANNELS";
+
+type UseMsTeamsChannelsProps = {
+  teamId: string;
+  queryOptions?: {};
+};
+
+type UseMsTeamsChannelsOutput = {
+  data: MsTeamsChannel[];
+  isLoading: boolean;
+  refetch: () => void;
+};
+
+function useMsTeamsChannels({
+  teamId,
+}: UseMsTeamsChannelsProps): UseMsTeamsChannelsOutput {
   const knock = useKnockClient();
+  const { knockMsTeamsChannelId, tenantId } = useKnockMsTeamsClient();
+
+  const fetchChannels = () => {
+    return knock.msTeams.getChannels({
+      knockChannelId: knockMsTeamsChannelId,
+      tenant: tenantId,
+      teamId,
+    });
+  };
+
+  const { data, isLoading, isValidating, mutate } =
+    useSWR<GetMsTeamsChannelsResponse>([QUERY_KEY], fetchChannels, {});
+
+  return {
+    data: data?.ms_teams_channels ?? [],
+    isLoading: isLoading || isValidating,
+    refetch: () => mutate(),
+  };
 }
 
 export default useMsTeamsChannels;
