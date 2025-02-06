6c04f9765b830555247a2d97162ffa70c460ca4a
Matthew Mikolay <mmikolay@knock.app>
2025-02-06 14:30:21 -0500

    Fix search font

--- packages/react/src/modules/slack/components/SlackChannelCombobox/SlackChannelCombobox.tsx
+++ packages/react/src/modules/slack/components/SlackChannelCombobox/SlackChannelCombobox.tsx
@@ -254,7 +254,7 @@ export const SlackChannelCombobox: FunctionComponent<
         >
           <Combobox.Trigger />
           <Combobox.Content>
-            <Combobox.Search />
+            <Combobox.Search className="rsk-combobox__search" />
             <Combobox.Options>
               {slackChannels.map((channel) => (
                 <Combobox.Option key={channel.id} value={channel.id}>
