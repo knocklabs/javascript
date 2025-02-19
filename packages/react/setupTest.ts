5a3c4b4d8d423e1c039b1da2a678e3f511593dd9
Matthew Mikolay <mmikolay@knock.app>
2025-02-19 17:16:09 -0500

    Fix type issues

--- packages/react/setupTest.ts
+++ packages/react/setupTest.ts
@@ -2,17 +2,18 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 import { expect } from "vitest";
+import type { AxeMatchers } from "vitest-axe/matchers";
 import * as matchers from "vitest-axe/matchers";
 
 expect.extend(matchers);
 
+declare module "vitest" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Assertion extends AxeMatchers {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
+
 afterEach(() => {
   cleanup();
 });
-
-/*
- fixes: Error: Not implemented: HTMLCanvasElement.prototype.getContext (without installing the canvas npm package)
- source: https://stackoverflow.com/questions/48828759/unit-test-raises-error-because-of-getcontext-is-not-implemented
-*/
-// @ts-expect-error -- see above
-HTMLCanvasElement.prototype.getContext = () => {};
