ad626145e1ab9a3fd9541671bddf95517246724b
Matthew Mikolay <mmikolay@knock.app>
2025-02-19 12:25:03 -0500

    Update EmptyFeed test

--- packages/react/test/feed/EmptyFeed.test.tsx
+++ packages/react/test/feed/EmptyFeed.test.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { describe, expect, test } from "vitest";
+import { axe } from "vitest-axe";
+import { toHaveNoViolations } from "vitest-axe/matchers";
 
 import { EmptyFeed } from "../../src";
 import { renderWithProviders } from "../test-utils";
 
-describe("Button", () => {
+expect.extend({ toHaveNoViolations });
+
+describe("EmptyFeed", () => {
   test("renders as expected", () => {
     const { container } = renderWithProviders(<EmptyFeed />);
     const emptyFeed = container.querySelector(".rnf-empty-feed");
 
     expect(emptyFeed).toHaveClass("rnf-empty-feed--light");
     expect(emptyFeed).toHaveTextContent("No notifications yet");
+  });
+
+  test("has no a11y violations", async () => {
+    const { container } = renderWithProviders(<EmptyFeed />);
+
+    expect(await axe(container)).toHaveNoViolations();
   });
 });
