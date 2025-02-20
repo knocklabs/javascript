16dfa4a7bb53051c854bd87fdd264081a6a5a067
Thomas <thomas.whyyou@gmail.com>
2025-02-20 01:21:54 -0500

    clean up ksuid helper package

--- packages/client/src/clients/guide/ksuid.ts
+++ packages/client/src/clients/guide/ksuid.ts
@@ -1,4 +1,10 @@
-// Copied over from https://github.com/ValeriaVG/xksuid
+// Copied over from https://github.com/ValeriaVG/xksuid to get around a build
+// error when installing and importing "xksuid" as a package:
+//
+//  > The current file is a CommonJS module whose imports will produce 'require'
+//  > calls; however, the referenced file is an ECMAScript module and cannot be
+//  > imported with 'require'.
+//
 /* eslint-disable */
 // @ts-nocheck
 
@@ -55,67 +61,6 @@ export function base62(view) {
 }
 
 /**
- * Decodes base62 string to buffer
- * @param {string} str
- * @returns {Uint8Array} buffer
- */
-export function debase62(str) {
-  if (str.length !== 27)
-    throw new Error("Expected 27 characters long base62 string");
-  const srcBase = 62n;
-  const dstBase = 4294967296n;
-  let bp = new Array(27);
-  const dst = new Uint8Array(20);
-  for (let i = 0; i < str.length; i++) {
-    bp[i] = str.charCodeAt(i);
-    // 0-9
-    if (bp[i] >= 48 && bp[i] <= 57) {
-      bp[i] -= 48; // '0'
-      continue;
-    }
-    // 10-35
-    if (bp[i] >= 65 && bp[i] <= 90) {
-      bp[i] = 10 + (bp[i] - 65);
-      continue;
-    }
-    // 36-61
-    if (bp[i] >= 97 && bp[i] <= 122) {
-      bp[i] = 36 + (bp[i] - 97);
-      continue;
-    }
-    throw new Error(`Unexpected symbol "${str.charAt(i)}"`);
-  }
-  let n = 20;
-  while (bp.length !== 0) {
-    let quotient = [];
-    let remainder = 0n;
-
-    for (const c of bp) {
-      let value = BigInt(c) + BigInt(remainder) * srcBase;
-      let digit = value / dstBase;
-      remainder = value % dstBase;
-
-      if (quotient.length !== 0 || digit !== 0n) {
-        quotient.push(Number(digit));
-      }
-    }
-
-    if (n < 4) {
-      throw new Error("short buffer");
-    }
-
-    dst[n - 4] = Number(remainder) >> 24;
-    dst[n - 3] = Number(remainder) >> 16;
-    dst[n - 2] = Number(remainder) >> 8;
-    dst[n - 1] = Number(remainder);
-    n -= 4;
-    bp = quotient;
-  }
-
-  return dst;
-}
-
-/**
  * Converts UNIX timestamp to (x)KSUID epoch timestamp
  * @param {number} timestamp ms
  * @param {boolean|undefined} desc order, `true` indicates xKSUID
@@ -126,19 +71,6 @@ export function toEpoch(timestamp, desc) {
     return Math.round(timestamp / 1000) - 14e8;
   }
   return 4294967295 - (Math.round(timestamp / 1000) - 14e8);
-}
-
-/**
- * Converts (x)KSUID epoch timestamp to UNIX timestamp
- * @param {number} timestamp s
- * @param {boolean|undefined} desc
- * @returns {number} ms
- */
-export function fromEpoch(timestamp, desc) {
-  if (!desc) {
-    return (14e8 + timestamp) * 1000;
-  }
-  return (4294967295 - timestamp + 14e8) * 1000;
 }
 
 /**
@@ -168,24 +100,4 @@ export function generate(desc = false, timestamp = Date.now()) {
   }
   if (desc) return "z" + base62(view);
   return base62(view);
-}
-
-/**
- * Parses (x)KSUID string to timestamp and random part
- * @param {string} ksuid
- * @return {{ts:Date,rnd:ArrayBuffer}} parsed value
- */
-export function parse(ksuid) {
-  if (ksuid.length > 28 || ksuid.length < 27) {
-    throw new Error(`Incorrect length: ${ksuid.length}, expected 27 or 28`);
-  }
-  const desc = ksuid.length === 28 && ksuid[0] == "z";
-  if (ksuid.length === 28 && ksuid[0] != "z") {
-    throw new Error(`KSUID is 28 symbol, but first char is not "z"`);
-  }
-  const buf = debase62(desc ? ksuid.slice(1, 28) : ksuid);
-  const view = new DataView(buf.buffer);
-  const tsValue = view.getUint32(0, false);
-  const ts = new Date(fromEpoch(tsValue, desc));
-  return { ts, rnd: buf.buffer.slice(4) };
 }
