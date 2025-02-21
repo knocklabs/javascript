8d6a8a456ec304de03e9ebccfef9e3e07e2bf7f7
Mike Carbone <mfcbone@gmail.com>
2025-02-21 15:27:57 -0500

    runs formatter

--- packages/react-core/src/modules/feed/hooks/useNotificationStore.ts
+++ packages/react-core/src/modules/feed/hooks/useNotificationStore.ts
@@ -26,41 +26,38 @@ function useCreateNotificationStore(feedClient: Feed) {
   return feedClient.store;
 }
 
-
 /**
  * Below we do some typing to specify that if a selector is provided,
  * the return type will be the type returned by the selector.
- * 
+ *
  * This is important because the store state type is not always the same as the
  * return type of the selector.
- * 
+ *
  */
 
 type StateSelector<T, U> = (state: T) => U;
 type FeedStoreStateSelector<T> = StateSelector<FeedStoreState, T>;
 
 // Function overload for when no selector is provided
-function useNotificationStore(
-  feedClient: Feed
-): FeedStoreState;
+function useNotificationStore(feedClient: Feed): FeedStoreState;
 
 // Function overload for when a selector is provided
 function useNotificationStore<T>(
   feedClient: Feed,
-  selector: FeedStoreStateSelector<T>
+  selector: FeedStoreStateSelector<T>,
 ): T;
 
 /**
  * A hook used to access content within the notification store.
- * 
+ *
  * A selector can be used to access a subset of the store state.
  * You can specify a specific subset type by importing `FeedStoreState` from the client package.
- * 
+ *
  * @example
- * 
+ *
  * ```ts
  * import { type FeedStoreState } from "@knocklabs/client";
- * 
+ *
  * const { items, metadata } = useNotificationStore(feedClient, (state) => ({
  *   items: state.items,
  *   metadata: state.metadata,
@@ -69,7 +66,7 @@ function useNotificationStore<T>(
  */
 function useNotificationStore<T>(
   feedClient: Feed,
-  selector?: FeedStoreStateSelector<T>
+  selector?: FeedStoreStateSelector<T>,
 ): T | FeedStoreState {
   const useStore = useCreateNotificationStore(feedClient);
   const storeState = useStore();
