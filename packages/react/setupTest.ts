45dfe5575d964387d53b5ca658f21bd03a8542c7
Matthew Mikolay <mmikolay@knock.app>
2025-03-20 09:35:21 -0400

    Clean up

--- packages/react/setupTest.ts
+++ packages/react/setupTest.ts
@@ -1,19 +1,6 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
-import { expect } from "vitest";
-// @ts-expect-error -- Something is wrong with the package, so this works for now
-import type { AxeMatchers } from "vitest-axe/matchers";
-import * as matchers from "vitest-axe/matchers";
-
-expect.extend(matchers);
-
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Assertion extends AxeMatchers {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
 
 afterEach(() => {
   cleanup();
