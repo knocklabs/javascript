30156e50168a60706703b0737c764f70c082c5c0
Matthew Mikolay <mmikolay@knock.app>
2025-03-20 12:07:09 -0400

    chore(KNO-7804): set up vitest-axe (#418)

--- packages/react/setupTest.ts
+++ packages/react/setupTest.ts
@@ -1,4 +1,7 @@
-import * as matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
 
-expect.extend(matchers);
+afterEach(() => {
+  cleanup();
+});
--- packages/react/test/feed/EmptyFeed.test.tsx
+++ packages/react/test/feed/EmptyFeed.test.tsx
@@ -1,15 +1,21 @@
-import React from "react";
 import { describe, expect, test } from "vitest";
 
 import { EmptyFeed } from "../../src";
+import { axe, expectToHaveNoViolations } from "../axe";
 import { renderWithProviders } from "../test-utils";
 
-describe("Button", () => {
+describe("EmptyFeed", () => {
   test("renders as expected", () => {
     const { container } = renderWithProviders(<EmptyFeed />);
     const emptyFeed = container.querySelector(".rnf-empty-feed");
 
     expect(emptyFeed).toHaveClass("rnf-empty-feed--light");
     expect(emptyFeed).toHaveTextContent("No notifications yet");
+  });
+
+  test("is accessible", async () => {
+    const { container } = renderWithProviders(<EmptyFeed />);
+
+    expectToHaveNoViolations(await axe(container));
   });
 });
