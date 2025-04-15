fb8590c33caba1f213f8633698758f9d613793fc
Kyle McDonald <kyle@knock.app>
2025-04-15 16:31:58 -0500

    fix: useNotifications memory leak

--- packages/react-core/src/modules/feed/hooks/useNotifications.ts
+++ packages/react-core/src/modules/feed/hooks/useNotifications.ts
@@ -1,5 +1,5 @@
 import Knock, { Feed, FeedClientOptions } from "@knocklabs/client";
-import { useMemo, useRef } from "react";
+import { useEffect, useState } from "react";
 
 import { useStableOptions } from "../../core";
 
@@ -8,29 +8,23 @@ function useNotifications(
   feedChannelId: string,
   options: FeedClientOptions = {},
 ) {
-  const feedClientRef = useRef<Feed>();
+  const [feedClient, setFeedClient] = useState<Feed>();
   const stableOptions = useStableOptions(options);
 
-  return useMemo(() => {
-    if (feedClientRef.current) {
-      feedClientRef.current.dispose();
-    }
+  useEffect(() => {
+    const newFeedClient = knock.feeds.initialize(feedChannelId, stableOptions);
 
-    feedClientRef.current = knock.feeds.initialize(
-      feedChannelId,
-      stableOptions,
-    );
+    newFeedClient.store.subscribe((t) => newFeedClient.store.setState(t));
 
-    // In development, we need to introduce this extra set state to force a render
-    // for Zustand as otherwise the state doesn't get reflected correctly
-    feedClientRef.current.store.subscribe((t) =>
-      feedClientRef?.current?.store.setState(t),
-    );
+    newFeedClient.listenForUpdates();
+    setFeedClient(newFeedClient);
 
-    feedClientRef.current.listenForUpdates();
-
-    return feedClientRef.current;
+    return () => {
+      newFeedClient.dispose();
+    };
   }, [knock, feedChannelId, stableOptions]);
+
+  return feedClient;
 }
 
 export default useNotifications;
