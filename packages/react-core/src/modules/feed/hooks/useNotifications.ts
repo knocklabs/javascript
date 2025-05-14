5beadc85514a35482e91d3869310ed09668a9b17
Matthew Mikolay <mmikolay@knock.app>
2025-05-14 13:37:01 -0400

    Use useState for feed client

--- packages/react-core/src/modules/feed/hooks/useNotifications.ts
+++ packages/react-core/src/modules/feed/hooks/useNotifications.ts
@@ -1,5 +1,5 @@
 import Knock, { Feed, FeedClientOptions } from "@knocklabs/client";
-import { useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { useStableOptions } from "../../core";
 
@@ -8,43 +8,30 @@ function useNotifications(
   feedChannelId: string,
   options: FeedClientOptions = {},
 ): Feed {
-  const feedClientRef = useRef<Feed>();
-  const disposedRef = useRef(false);
   const stableOptions = useStableOptions(options);
-
-  useEffect(() => {
-    return () => {
-      const isDisposed = disposedRef.current;
-      if (!isDisposed) {
-        feedClientRef.current?.dispose();
-        disposedRef.current = true;
-      }
-    };
-  }, []);
-
-  return useMemo(() => {
-    const isDisposed = disposedRef.current;
-    if (!isDisposed && feedClientRef.current) {
-      feedClientRef.current.dispose();
-      disposedRef.current = true;
-    }
-
-    feedClientRef.current = knock.feeds.initialize(
-      feedChannelId,
-      stableOptions,
-    );
-    disposedRef.current = false;
+  const initFeedClient = useCallback(() => {
+    const feedClient = knock.feeds.initialize(feedChannelId, stableOptions);
 
     // In development, we need to introduce this extra set state to force a render
     // for Zustand as otherwise the state doesn't get reflected correctly
-    feedClientRef.current.store.subscribe((t) =>
-      feedClientRef?.current?.store.setState(t),
-    );
+    feedClient.store.subscribe((t) => feedClient.store.setState(t));
 
-    feedClientRef.current.listenForUpdates();
+    feedClient.listenForUpdates();
 
-    return feedClientRef.current;
+    return feedClient;
   }, [knock, feedChannelId, stableOptions]);
+
+  const [feedClient, setFeedClient] = useState<Feed | null>(null);
+
+  useEffect(() => {
+    const nextFeedClient = initFeedClient();
+    setFeedClient(nextFeedClient);
+    return () => {
+      nextFeedClient.dispose();
+    };
+  }, [initFeedClient]);
+
+  return feedClient;
 }
 
 export default useNotifications;
