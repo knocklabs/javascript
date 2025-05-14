bdbe86386b0bddcebc845c750786217ae65f9064
Matthew Mikolay <mmikolay@knock.app>
2025-05-14 16:00:11 -0400

    Check feedId and options to avoid unnecessary reinits

--- packages/react-core/src/modules/feed/hooks/useNotifications.ts
+++ packages/react-core/src/modules/feed/hooks/useNotifications.ts
@@ -3,35 +3,54 @@ import { useCallback, useEffect, useState } from "react";
 
 import { useStableOptions } from "../../core";
 
+interface State {
+  feedClient: Feed;
+  options: FeedClientOptions;
+}
+
 function useNotifications(
   knock: Knock,
   feedChannelId: string,
   options: FeedClientOptions = {},
 ): Feed {
+  const initFeedClient = useCallback(
+    (feedChannelId: string, options: FeedClientOptions) => {
+      const feedClient = knock.feeds.initialize(feedChannelId, options);
+
+      // In development, we need to introduce this extra set state to force a render
+      // for Zustand as otherwise the state doesn't get reflected correctly
+      feedClient.store.subscribe((t) => feedClient.store.setState(t));
+
+      feedClient.listenForUpdates();
+
+      return feedClient;
+    },
+    [knock],
+  );
+
   const stableOptions = useStableOptions(options);
-  const initFeedClient = useCallback(() => {
-    const feedClient = knock.feeds.initialize(feedChannelId, stableOptions);
-
-    // In development, we need to introduce this extra set state to force a render
-    // for Zustand as otherwise the state doesn't get reflected correctly
-    feedClient.store.subscribe((t) => feedClient.store.setState(t));
-
-    feedClient.listenForUpdates();
-
-    return feedClient;
-  }, [knock, feedChannelId, stableOptions]);
-
-  const [feedClient, setFeedClient] = useState<Feed | null>(null);
+  const [state, setState] = useState<State>(() => ({
+    feedClient: initFeedClient(feedChannelId, stableOptions),
+    options: stableOptions,
+  }));
 
   useEffect(() => {
-    const nextFeedClient = initFeedClient();
-    setFeedClient(nextFeedClient);
-    return () => {
-      nextFeedClient.dispose();
-    };
-  }, [initFeedClient]);
+    const needsReinit =
+      state.feedClient.feedId !== feedChannelId ||
+      state.options !== stableOptions;
 
-  return feedClient;
+    if (needsReinit) {
+      setState({
+        feedClient: initFeedClient(feedChannelId, stableOptions),
+        options: stableOptions,
+      });
+      return;
+    }
+
+    return () => state.feedClient.dispose();
+  }, [initFeedClient, feedChannelId, stableOptions, state]);
+
+  return state.feedClient;
 }
 
 export default useNotifications;
