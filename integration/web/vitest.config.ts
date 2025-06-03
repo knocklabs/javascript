6107997a5beb0b9a19ffc111c0737ae3674b44e0
Kyle McDonald <kyle@knock.app>
2025-06-03 15:40:17 -0500

    Revert "fix: change how integration is setup"
    This reverts commit a1a4649d5dd711fedca989dc828183120b23be89.

--- integration/web/vitest.config.ts
+++ integration/web/vitest.config.ts
@@ -2,14 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    coverage: {
-      provider: "v8",
-      all: true,
-      include: ["src/**"],
-      exclude: ["dist/**", "node_modules/**"],
-    },
     globals: true,
     environment: "jsdom",
-    setupFiles: ["dotenv/config"],
+    setupFiles: ["dotenv/config", "./setupTest.ts"],
   },
 });
---
+++ vitest/integration/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config";
+
+const sharedConfig = defineConfig({
+  test: {
+    coverage: {
+      provider: "v8",
+      all: true,
+      include: ["integration/*/src/**"],
+      exclude: [
+        "**/*.test.ts",
+        "**/test/**",
+        "*.d.ts",
+        "**/dist/**",
+        "**/node_modules/**",
+      ],
+    },
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["dotenv/config"],
+  },
+});
+export default sharedConfig;
