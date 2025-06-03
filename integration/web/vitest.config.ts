d6ff8a1d45f2409cb8153544cd9942a656b827b3
Kyle McDonald <kyle@knock.app>
2025-06-03 15:34:04 -0500

    fix: change how integration is setup

--- integration/web/vitest.config.ts
+++ integration/web/vitest.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "v8",
+      all: true,
+      include: ["src/**"],
+      exclude: ["dist/**", "node_modules/**"],
+    },
     globals: true,
     environment: "jsdom",
-    setupFiles: ["dotenv/config", "./setupTest.ts"],
+    setupFiles: ["dotenv/config"],
   },
 });
