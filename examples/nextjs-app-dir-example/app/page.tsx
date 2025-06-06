3d2c0bb5baac8e027d5630a437e0b49f0f284a58
Kyle McDonald <kyle@knock.app>
2025-06-06 10:51:17 -0500

    feat: add use client directive to @knocklabs/react + example app directory app

---
+++ examples/nextjs-app-dir-example/app/page.tsx
@@ -0,0 +1,28 @@
+import {
+  KnockFeedProvider,
+  KnockProvider,
+  NotificationFeed,
+} from "@knocklabs/react";
+import { Box } from "@telegraph/layout";
+
+/**
+ * Display the Knock Notification Feed without the need of the "use client" directive.
+ */
+
+export default async function Home() {
+  return (
+    <Box maxW="160" border="px" rounded="3" p="4" mx="auto" mt="8">
+      <KnockProvider
+        userId={process.env.NEXT_PUBLIC_KNOCK_USER_ID!}
+        apiKey={process.env.NEXT_PUBLIC_KNOCK_PUBLIC_API_KEY!}
+        host={process.env.NEXT_PUBLIC_KNOCK_HOST}
+      >
+        <KnockFeedProvider
+          feedId={process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID!}
+        >
+          <NotificationFeed />
+        </KnockFeedProvider>
+      </KnockProvider>
+    </Box>
+  );
+}
---
+++ examples/nextjs-app-dir-example/components/NotificationToasts.tsx
@@ -0,0 +1,45 @@
+import { type FeedItem } from "@knocklabs/client";
+import { useKnockFeed } from "@knocklabs/react";
+import { useCallback, useEffect } from "react";
+import { toast } from "sonner";
+
+const NotificationToasts = () => {
+  const { feedClient } = useKnockFeed();
+
+  const onNotificationsReceived = useCallback(
+    ({ items }: { items: FeedItem[] }) => {
+      // Whenever we receive a new notification from our real-time stream, show a toast
+      // (note here that we can receive > 1 items in a batch)
+      items.forEach((notification) => {
+        if (notification.data?.showToast === false) return;
+
+        // You can access the Knock notification data
+        const description = notification.data?.message;
+
+        // Handle the notification however you want
+        toast.success("New Notification Received", {
+          description: description,
+          closeButton: true,
+          dismissible: true,
+          onDismiss: () => {
+            feedClient.markAsSeen(notification);
+          },
+        });
+      });
+    },
+    [feedClient, toast],
+  );
+
+  useEffect(() => {
+    // Receive all real-time notifications on our feed
+    feedClient.on("items.received.realtime", onNotificationsReceived);
+
+    // Cleanup
+    return () =>
+      feedClient.off("items.received.realtime", onNotificationsReceived);
+  }, [feedClient, onNotificationsReceived]);
+
+  return null;
+};
+
+export default NotificationToasts;
