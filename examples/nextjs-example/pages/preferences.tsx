f237318eb9e53dc01f4a50377cf0ef0cbc0ce6d2
Kyle McDonald <kyle@knock.app>
2025-07-16 10:22:10 -0500

    fix: incorrect comment

--- examples/nextjs-example/pages/preferences.tsx
+++ examples/nextjs-example/pages/preferences.tsx
@@ -8,7 +8,7 @@ const Preferences = () => {
   const { userId, userToken } = useIdentify();
 
   const tokenRefreshHandler = useCallback(async () => {
-    // Refresh the user token 1s before it expires
+    // Refresh the user token 5s before it expires
     const res = await fetch(`/api/auth?id=${userId}`);
     const json = await res.json();
 
