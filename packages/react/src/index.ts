fe981090f5c333faed429b21a4bed169220f1712
Connor Lindsey <cblindsey3@gmail.com>
2025-08-19 12:57:44 -0700

    stub out component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -42,6 +42,8 @@ export {
   BannerView,
   Card,
   CardView,
+  GuideDevTools,
+  KnockGuideProvider,
   Modal,
   ModalView,
 } from "./modules/guide";
@@ -88,7 +90,6 @@ export {
   useKnockFeed,
   useNotificationStore,
   useNotifications,
-  KnockGuideProvider,
   KnockGuideContext,
   useGuide,
   useGuides,
