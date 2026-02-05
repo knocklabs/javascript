0d930245bdf7439a8163f4e95ee7eda635bcbeb4
Thomas <thomas.whyyou@gmail.com>
2026-02-05 15:12:54 -0500

    update z-index value to what's used in tooltip

--- packages/react/src/modules/guide/components/Toolbar/shared.ts
+++ packages/react/src/modules/guide/components/Toolbar/shared.ts
@@ -1,3 +1,2 @@
-// 'max' z index based on max value of a signed 32-bit int
-// Ref: https://stackoverflow.com/questions/491052/minimum-and-maximum-value-of-z-index/25461690#25461690
-export const TOOLBAR_Z_INDEX = 2147483647;
+// Match the hardcoded z-index value in telegraph tooltip for now.
+export const TOOLBAR_Z_INDEX = 1800;
