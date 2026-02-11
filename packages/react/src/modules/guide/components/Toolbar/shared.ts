a4d0d6da33e1632a65a430e720ca02b1597a1e26
Thomas <thomas.whyyou@gmail.com>
2026-02-11 16:38:31 -0500

    comment update

--- packages/react/src/modules/guide/components/Toolbar/shared.ts
+++ packages/react/src/modules/guide/components/Toolbar/shared.ts
@@ -1,2 +1,3 @@
-// Match the hardcoded z-index value in telegraph tooltip for now.
+// Match the hardcoded z-index value in the telegraph tooltip package for now
+// to ensure everything within a toolbar is visible.
 export const TOOLBAR_Z_INDEX = 1800;
