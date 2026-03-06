47b42a5ac5484875ea9f4c3dfd95fd732776ec6a
Thomas <thomas.whyyou@gmail.com>
2026-03-06 11:29:32 -0500

    cursor review feedback

--- packages/react/src/modules/guide/components/Toolbar/V2/GuideAnnotatedStatusDot.tsx
+++ packages/react/src/modules/guide/components/Toolbar/V2/GuideAnnotatedStatusDot.tsx
@@ -68,9 +68,7 @@ export const GuideAnnotatedStatusDot = ({
         p="0_5"
         style={{ flexShrink: 0 }}
       >
-        <svg width="8" height="8" viewBox="0 0 8 8" aria-hidden>
-          {STATUS_SHAPES[color]}
-        </svg>
+        {STATUS_SHAPES[color]}
       </Stack>
     </Tooltip>
   );
