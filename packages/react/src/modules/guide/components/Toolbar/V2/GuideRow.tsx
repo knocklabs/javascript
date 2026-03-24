2c813bec34f0f1111cb577238e3280d8d5fb0a5a
Thomas <thomas.whyyou@gmail.com>
2026-03-24 18:22:01 -0400

    better handling of focus error reason and message

--- packages/react/src/modules/guide/components/Toolbar/V2/GuideRow.tsx
+++ packages/react/src/modules/guide/components/Toolbar/V2/GuideRow.tsx
@@ -13,6 +13,7 @@ import {
   GuideAnnotatedStatusDot as StatusDot,
 } from "./GuideAnnotatedStatusDot";
 import { GuideRowDetails } from "./GuideRowDetails";
+import { ERROR_MESSAGE } from "./helpers";
 import {
   AnnotatedGuide,
   AnnotatedStatuses,
@@ -354,12 +355,15 @@ export const GuideRow = ({ guide, orderIndex, isExpanded, onClick }: Props) => {
 
           <Tooltip
             label={
+              isUncommittedGuide(guide)
+                ? ERROR_MESSAGE.focusUncommittedGuide
+                : guide.annotation.selectable.status === undefined
+                  ? ERROR_MESSAGE.focusUnselectableGuide
+                  : ""
+            }
+            enabled={
               isUncommittedGuide(guide) ||
               guide.annotation.selectable.status === undefined
-                ? "No component found that can render this guide"
-                : isFocused
-                  ? "Unfocus this guide"
-                  : "Focus on this guide"
             }
           >
             <Button
